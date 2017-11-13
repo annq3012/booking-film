e75590d8b76bcc9c55a2a29d2392dc4d042eee86
at-annguyen <ngqanlc1995@gmail.com>
2017-11-13 10:09:28 +0700

    Migration

---
+++ app/Model/BookingFilm.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class BookingFilm extends Model
+{
+     /**
+     * Declare table
+     *
+     * @var string $tabel table name
+     */
+    protected $table = 'booking_films';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array $fillable
+     */
+    protected $fillable = [
+        'booking_time', 'user_id', 'schedule_id'
+    ];
+
+     /**
+     * Relationship with user
+     *
+     * @return \App\Model
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    /**
+     * Relationship hasMany with detail booking
+     *
+     * @return array
+    */
+    public function detailBooking()
+    {
+        return $this->hasMany(DetailBooking::class, 'booking_film_id');
+    }
+
+     /**
+     * Relationship with schedule
+     *
+     * @return \App\Model
+     */
+    public function schedule()
+    {
+        return $this->belongsTo(Schedule::class, 'schedule_id');
+    }
+}
---
+++ database/migrations/2017_11_09_085709_create_films_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFilmsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('films', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->text('description');
+            $table->string('language');
+            $table->string('director');
+            $table->string('actor');
+            $table->string('year');
+            $table->string('duration');
+            $table->string('image')->nullable();
+            $table->string('release');
+            $table->string('genre');
+            $table->string('technologies');
+            $table->string('rated');
+            $table->string('link')->nullable();
+            $table->tinyInteger('status');
+            $table->string('slug')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('films');
+    }
+}

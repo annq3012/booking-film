aa1f1afeac26e839a736eeffe771d007144d936b
at-annguyen <ngqanlc1995@gmail.com>
2017-12-07 09:59:50 +0700

    Update Film

--- app/Http/Controllers/Admin/FilmController.php
+++ app/Http/Controllers/Admin/FilmController.php
@@ -19,4 +19,54 @@ class FilmController extends Controller
                       ->paginate(Film::ROW_LIMIT);
          return view('backend.films.index', compact('films'));
     }
+
+    /**
+    * Show the form for editing the specified resource.
+    *
+    * @param int $film object of film
+    *
+    * @return \Illuminate\Http\Response
+    */
+    public function edit(Film $film)
+    {
+        $technologies = $film::$technologies;
+        $rated = $film::$rated;
+        $actived = $film::STATUS_ACTIVED;
+        $disabled = $film::STATUS_DISABLED;
+        return view(
+            'backend.films.create',
+            compact(
+                'technologies',
+                'rated',
+                'actived',
+                'disabled'
+            )
+        );
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\UpdateRequest $request request to update
+     * @param int                            $film    object of film
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateFilmRequest $request, film $film)
+    {
+        /*$input = $request->all();
+        if ($request->hasFile('image')) {
+            $file = $request->file('image');
+            $fileName = config('image.name_prefix') . "-" . $file->hashName();
+            $file->move(config('image.films.path_upload'), $fileName);
+            $input['image'] = $fileName;
+        }
+        if ($film->update($input)) {
+            flash(__('Update successful!'))->success();
+            return redirect()->route('users.index');
+        } else {
+            flash(__('Update failed!'))->error();
+            return redirect()->back()->withInput();
+        }*/
+    }
 }
---
+++ resources/views/backend/films/update.blade.php
@@ -0,0 +1,121 @@
+@extends('backend.layout.main')
+
+@section('title', __('Films Update Page'))
+
+@section('content')
+<div class="content-wrapper">
+  <div class="box box-primary">
+            <div >
+              <h3 class="h-title" >{{ __('Films Update') }}</h3>
+              @include('flash::message')
+            </div>
+            <!-- /.box-header -->
+            <!-- form start -->
+            <form role="form" method="POST" action="{{ route('films.store') }}" enctype="multipart/form-data">
+              {{ csrf_field()}}
+              <div class="box-body">
+                <div class="form-group">
+                  <label for="name">{{ __('Name') }}</label>
+                  <input type="text" class="form-control {{ $errors->has('name') ? ' has-error' : '' }}" id="name" name="name" placeholder="{{ __('Enter your name') }}" value="{{ old('name', $film->name) }}">
+                  <small class="text-danger">{{ $errors->first('name') }}</small>
+                </div>
+                 <div class="form-group pull-left">
+                  <label for="language">{{ __('Language') }}</label>
+                  <input type="text" class="form-control {{ $errors->has('language') ? ' has-error' : '' }}" id="language" name="language" placeholder="{{ __('Enter language') }}" value="{{ old('language', $film->language) }}">
+                  <small class="text-danger">{{ $errors->first('language') }}</small>
+                </div>
+                <div class="form-group pull-left ml-10">
+                  <label for="actor">{{ __('Actor') }}</label>
+                  <input type="text" class="form-control {{ $errors->has('actor') ? ' has-error' : '' }}" id="actor" name="actor" placeholder="{{ __('Enter actor') }}" value="{{ old('actor', $film->actor) }}">
+                  <small class="text-danger">{{ $errors->first('actor') }}</small>
+                </div>
+                <div class="form-group pull-left ml-10">
+                  <label for="director">{{ __('Director') }}</label>
+                  <input type="text" class="form-control {{ $errors->has('director') ? ' has-error' : '' }}" id="director" name="director" placeholder="{{ __('Enter director') }}" value="{{ old('director', $film->director) }}">
+                  <small class="text-danger">{{ $errors->first('director') }}</small>
+                </div>
+                <div class="form-group pull-left ml-10">
+                  <label for="duration">{{ __('Genre') }}</label>
+                  <input type="text" class="form-control {{ $errors->has('genre') ? ' has-error' : '' }}" id="genre" name="genre" placeholder="{{ __('Enter genre') }}">
+                  <small class="text-danger">{{ $errors->first('genre') }}</small>
+                </div>
+                <div class="form-group clr pull-left ml-10 ">
+                  <label for="technologies">{{ __('Technologies') }}</label>
+                  <select name="technologies" id="technologies" class="{{ $errors->has('type') ? ' has-error' : '' }}">
+                    @foreach ($technologies as $technology)
+                     <option value="{{$technology}}" {{$technology == old('technologies', $film->technologies )? 'selected' : ''}}>{{$technology}}</option>
+                    @endforeach
+                  </select>
+                </div>
+                <div class="form-group pull-left ml-10">
+                  <label for="rated">{{ __('Rated') }}</label>
+                  <select name="rated" id="rated">
+                    @foreach ($rated as $rated)
+                      <option value="{{$rated}}" {{$rated == old('rated', $film->rated )? 'selected' : ''}}>{{$rated}}</option>
+                    @endforeach
+                  </select>
+                </div>
+                <div class="form-group pull-left ml-10">
+                  <label for="year">{{ __('Years') }}</label>
+                    @php
+                      $years = (int) date('Y');
+                    @endphp
+                  <select name="year" id="year">
+                    @for ($i = $years; $i <= $years+2 ; $i++)
+                      <option value="{{$i}}">{{ $i }}</option>
+                    @endfor
+                  </select>
+                </div>
+                <div class="form-group pull-left ml-10">
+                  <label>{{ __('Release') }}:</label>
+                  <div class="input-group">
+                    <div class="input-group-addon">
+                      <i class="fa fa-calendar"></i>
+                    </div>
+                    <input type="date" class="form-control {{ $errors->has('release') ? ' has-error' : '' }}" name="release" value="{{ old('release') }}">
+                  </div>
+                   <small class="text-danger">{{ $errors->first('release') }}</small>
+                  <!-- /.input group -->
+                </div>
+                <div class="form-group clr">
+                  <label for="duration">{{ __('Duration') }}</label>
+                  <input type="text" class="form-control {{ $errors->has('duration') ? ' has-error' : '' }}" id="duration" name="duration" placeholder="{{ __('Enter duration') }}" value="{{ old('duration') }}">
+                  <small class="text-danger">{{ $errors->first('duration') }}</small>
+                </div>
+                <div class="form-group">
+                  <label for="description">{{ __('Description') }}</label>
+                  <textarea class="form-control ckeditor {{ $errors->has('description') ? ' has-error' : '' }}" id="description" name="description" placeholder="{{ __('Enter description') }}"></textarea>
+                  <small class="text-danger">{{ $errors->first('description') }}</small>
+                </div>
+                <div class="form-group">
+                  <label for="image">{{ __('File input')}}</label>
+                  <input type="file" id="image" name="image">
+                </div>
+                 <div class="form-group">
+                  <label for="link">{{ __('Link') }}</label>
+                  <input type="text" class="form-control {{ $errors->has('link') ? ' has-error' : '' }}" id="link" name="link" placeholder="{{ __('Enter link') }}" value="{{ old('link') }}">
+                  <small class="text-danger">{{ $errors->first('link') }}</small>
+                </div>
+                <div class="form-group">
+                  <label>
+                    <label for="admin">Actived</label>
+                    <input type="radio" id="actived" class="flat-red" name="status" checked value="{{$actived}}">
+                  </label>
+                  <label>
+                    <label for="user">Disable</label>
+                    <input type="radio" id="disabled" class="flat-red" name="status" value="{{$disabled}}">
+                  </label>
+                </div>
+              </div>
+              <!-- /.box-body -->
+
+              <div class="box-footer">
+                <button type="submit" class="btn btn-primary pull-right">{{ __('Submit') }}</button>
+                <a href="{{ route('films.index') }}" class="btn btn-default pull-left">{{ __('Back') }}</a>
+                <button type="reset" class="btn btn-warning pull-left btn-reset">{{ __('Reset') }}</button>
+              </div>
+            </form>
+          </div>
+          <div class="row"></div>
+</div>
+@endsection

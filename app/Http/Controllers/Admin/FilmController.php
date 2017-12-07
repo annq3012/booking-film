bbdfeb8167b8263a68086db1acc634d55228d0a2
at-annguyen <ngqanlc1995@gmail.com>
2017-12-07 09:48:33 +0700

    Delete Films

--- app/Http/Controllers/Admin/FilmController.php
+++ app/Http/Controllers/Admin/FilmController.php
@@ -19,4 +19,24 @@ class FilmController extends Controller
                       ->paginate(Film::ROW_LIMIT);
          return view('backend.films.index', compact('films'));
     }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param int $film object of film
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Film $film)
+    {
+        if ($film->image != null) {
+            unlink(assert('images/films'.$film->image));
+        }
+        if ($film->delete()) {
+            flash(__('Deletion successful!'))->success();
+        } else {
+            flash(__('Deletion failed!'))->error();
+        }
+        return redirect()->route('films.index');
+    }
 }
